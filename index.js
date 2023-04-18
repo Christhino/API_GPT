6389258479dc7ab7791f3825d114ded4174694dc
Christhino <mintsaniaina.christhino@gmail.com>
2023-04-19 01:41:50 +0300

    feat/ auth

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const createError = require("http-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const session = require('express-session');
 const SibAPIV3Sdk = require('sib-api-v3-sdk')
 const { v4: uuidv4 } = require('uuid');
 const shortid = require('shortid');
@@ -57,9 +57,18 @@ var apiKey  = defaultClient.authentications['api-key']
 apiKey.apiKey = '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'
 
 
+app.use(session({
+  secret: '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',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+const verificationCode = Math.floor(100000 + Math.random() * 900000);
+
 app.post('/api/login' ,async(req,res) => {
   const apiInstance =  new  SibAPIV3Sdk.TransactionalEmailsApi();
-  const verificationCode = shortid.generate();
+  // const verificationCode = Math.floor(100000 + Math.random() * 900000);
+
   const sender = {
     email: "mintsaniaina.christhino.pro@gmail.com",
     name:"Verbalia"
@@ -83,6 +92,18 @@ app.post('/api/login' ,async(req,res) => {
   }
   
 })
+
+app.post('/api/confirm', async (req, res) => {
+  const userCode = req.body.code; // le code envoyé par l'utilisateur
+  // vérifier si le code est valide
+  if (userCode === verificationCode) {
+    return res.send('Code is valid');
+  } else {
+    return res.status(400).send('Invalid code');
+  }
+});
+
+
 app.post('/test', async (req, res) => {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
