86dfc9e6e95968661d137d7937669f76b534aa2d
Christhino <mintsaniaina.christhino@gmail.com>
2023-04-27 14:33:40 +0300

    change format

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post('/api/confirm', async (req, res) => {
 app.post('/test', async (req, res) => {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: "text-curie-001",
       prompt: prompt,
       temperature: 0.7,
       max_tokens: 60,
